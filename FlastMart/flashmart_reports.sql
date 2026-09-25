7163658406c1ac7ec93a7b510eafa7f97a7b483b
nvmanh206 <dtc245200223@ictu.edu.vn>
2026-09-25 09:29:24 +0700

    Complete FlashMart JOIN assignment

--- FlastMart/flashmart_reports.sql
+++ FlastMart/flashmart_reports.sql
@@ -12,3 +12,10 @@ FROM Customers c
 LEFT JOIN Orders o
     ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name;
+SELECT
+    p.product_id,
+    p.product_name
+FROM Products p
+LEFT JOIN Orders o
+    ON p.product_id = o.product_id
+WHERE o.order_id IS NULL;

f29a636f3f6ad4f7222a4205c020b9d697ff98bd
nvmanh206 <dtc245200223@ictu.edu.vn>
2026-09-25 08:37:27 +0700

    Complete SQL student management insert exercise

--- studen_manager_insert/student_management_insert_.sql
+++ studen_manager_insert/student_management_insert_.sql
@@ -1,50 +1,17 @@
-CREATE DATABASE IF NOT EXISTS QuanLySinhVien;
 USE QuanLySinhVien;
 
-CREATE TABLE Class (
-    ClassID INT NOT NULL AUTO_INCREMENT,
-    ClassName VARCHAR(60) NOT NULL,
-    StartDate DATETIME NOT NULL,
-    Status BIT,
-    PRIMARY KEY (ClassID)
-);
-
-CREATE TABLE Student (
-    StudentID INT NOT NULL AUTO_INCREMENT,
-    StudentName VARCHAR(30) NOT NULL,
-    Address VARCHAR(50),
-    Phone VARCHAR(20),
-    Status BIT,
-    ClassID INT NOT NULL,
-    PRIMARY KEY (StudentID),
-    FOREIGN KEY (ClassID) REFERENCES Class(ClassID)
-);
-
-CREATE TABLE Subject (
-    SubID INT NOT NULL AUTO_INCREMENT,
-    SubName VARCHAR(30) NOT NULL,
-    Credit TINYINT NOT NULL DEFAULT 1,
-    Status BIT DEFAULT 1,
-    PRIMARY KEY (SubID)
-);
-
-CREATE TABLE Mark (
-    MarkID INT NOT NULL AUTO_INCREMENT,
-    SubID INT NOT NULL,
-    StudentID INT NOT NULL,
-    Mark FLOAT DEFAULT 0,
-    ExamTimes TINYINT DEFAULT 1,
-    PRIMARY KEY (MarkID),
-    FOREIGN KEY (SubID) REFERENCES Subject(SubID),
-    FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
-);
+-- Bảng Class
+INSERT INTO Class
+VALUES (1, 'A1', '2008-12-20', 1);
 
 INSERT INTO Class
-VALUES
-(1, 'A1', '2008-12-20', 1),
-(2, 'A2', '2008-12-22', 1),
-(3, 'B3', CURRENT_DATE, 0);
+VALUES (2, 'A2', '2008-12-22', 1);
 
+INSERT INTO Class
+VALUES (3, 'B3', CURRENT_DATE, 0);
+
+
+-- Bảng Student
 INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
 VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
 
@@ -54,19 +21,25 @@ VALUES ('Hoa', 'Hai phong', 1, 1);
 INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
 VALUES ('Manh', 'HCM', '0123123123', 0, 2);
 
+
+-- Bảng Subject
 INSERT INTO Subject
 VALUES
-(1, 'CF', 5, 1),
-(2, 'C', 6, 1),
-(3, 'HDJ', 5, 1),
-(4, 'RDBMS', 10, 1);
+    (1, 'CF', 5, 1),
+    (2, 'C', 6, 1),
+    (3, 'HDJ', 5, 1),
+    (4, 'RDBMS', 10, 1);
 
+
+-- Bảng Mark
 INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
 VALUES
-(1, 1, 8, 1),
-(1, 2, 10, 2),
-(2, 1, 12, 1);
+    (1, 1, 8, 1),
+    (1, 2, 10, 2),
+    (2, 1, 12, 1);
 
+
+-- Kiểm tra dữ liệu
 SELECT * FROM Class;
 SELECT * FROM Student;
 SELECT * FROM Subject;
